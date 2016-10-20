f6398790cd11a37c35c80fbd1fed1bb2d47cc6c7
sssaravinth <sssaravinth>
2016-10-20 18:41:16 +0530

    hi

---
+++ src/main/java/com/aravinth/HelloWorld.java
@@ -0,0 +1,34 @@
+package com.aravinth;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+ 
+@Controller
+
+public class HelloWorld {
+	String message = "Welcome to Spring MVC!";
+	 
+	@RequestMapping("/")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("index");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+	@RequestMapping("/helloworld")
+	public ModelAndView showMessage2(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+		System.out.println("in controller");
+ 
+		ModelAndView mv = new ModelAndView("helloworld");
+		mv.addObject("message", message);
+		mv.addObject("name", name);
+		return mv;
+	}
+
+}
